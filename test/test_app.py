d31b88949a2e88cca73afbaf3a80aebd3e70f321
SandeepkrGoyal <sandeepgoyal1804@gmail.com>
2021-05-15 13:21:40 +0530

    Added Youtube Title

--- test/test_app.py
+++ test/test_app.py
@@ -4,10 +4,20 @@ import pytest
 
 class Testapp(Driverutility):
     @pytest.mark.regression
-    def test_chrome(self):
+    def test_amazon(self):
         headless_status = super().parse_config("project_config", "browser_config", "headless_status")
         driver = super().set_driver("chrome", headless_status)
         driver.get("https://www.amazon.in/")
+        get_title = driver.title
+        print(get_title)
+        assert get_title == "YouTube", "Title does not match"
+        super().tear_down(driver)
+
+    @pytest.mark.regression
+    def test_youtube(self):
+        headless_status = super().parse_config("project_config", "browser_config", "headless_status")
+        driver = super().set_driver("chrome", headless_status)
+        driver.get("https://www.youtube.com/")
         get_title = driver.title
         print(get_title)
         assert get_title == "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in", "Title does not match"
